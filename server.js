655ff7d29472811a8c4b5ef3cecd31de4f0f8805
honeykhra <honeykhra@gmail.com>
2023-02-20 17:14:01 +0530

    fixed server warning

--- server.js
+++ server.js
@@ -1,47 +1,47 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-//env config 
-require('dotenv').config();
+//env config
+require("dotenv").config();
 
 //allow access for 3rd partywebsite.
 app.use(cors());
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //application/x-www-form-urlencoded.
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //db connection.
 (function dbConnect() {
-    const db = require('./models');
+  const db = require("./models");
 
-    try {
-        db.mongoose.connect(db.url, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        console.log('DB connected successfully');
-    }
-    catch (err) {
-        console.log('Failed to connect a DB', err);
-        process.exit(1);
-    }
-})()
+  try {
+    db.mongoose.set("strictQuery", false);
+    db.mongoose.connect(db.url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("DB connected successfully");
+  } catch (err) {
+    console.log("Failed to connect a DB", err);
+    process.exit(1);
+  }
+})();
 
 //test connection
-app.get('/', (req, res) => {
-    res.status(200).send({
-        message: 'Welcome to best buddy web server.'
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message: "Welcome to best buddy web server.",
+  });
+});
 
 //user routes
-require('./routes/user.routes')(app);
+require("./routes/user.routes")(app);
 
-app.listen(PORT, () => console.log(`server is running on ${PORT}`))
+app.listen(PORT, () => console.log(`server is running on ${PORT}`));
