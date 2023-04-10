80c905e2720ba810155c04568d55be8f9cf7b90a
honeykhra <honeykhra@gmail.com>
2023-04-10 21:20:07 +0530

    city and preferences fields added in uer schema

--- models/property.model.js
+++ models/property.model.js
@@ -1,26 +1,26 @@
 module.exports = (mongoose) => {
-    const properties = mongoose.model(
-        'properties',
-        mongoose.Schema(
-            {
-                user_id: { type: mongoose.Schema.Types.ObjectId, required: true },
-                user_name: { type: String, required: true },
-                name: { type: String, required: true },
-                city: { type: String, required: true },
-                phone: { type: Number, required: true },
-                rent: { type: Number, required: true },
-                details: { type: String },
-                gender: { type: String, required: true },
-                category: { type: String, required: true },
-                availableDate: { type: String, required: true },
-                typeOfShare: { type: String, require: true },
-                amenties: { type: [String], required: true },
-                perfrences: { type: [String], required: true },
-                image: { type: [String], required: true }
-            },
-            { timestamps: true }
-        )
+  const properties = mongoose.model(
+    "properties",
+    mongoose.Schema(
+      {
+        user_id: { type: mongoose.Schema.Types.ObjectId, required: true },
+        user_name: { type: String, required: true },
+        name: { type: String, required: true },
+        city: { type: String, required: true },
+        phone: { type: Number, required: true },
+        rent: { type: Number, required: true },
+        details: { type: String },
+        gender: { type: String, required: true },
+        category: { type: String, required: true },
+        availableDate: { type: String, required: true },
+        typeOfShare: { type: String, require: true },
+        amenties: { type: [String], required: true },
+        preferences: { type: [String], required: true },
+        image: { type: [String], required: true },
+      },
+      { timestamps: true }
     )
+  );
 
-    return properties;
-}
+  return properties;
+};
--- models/user.model.js
+++ models/user.model.js
@@ -1,19 +1,21 @@
 module.exports = (mongoose) => {
-    const users = mongoose.model(
-        'users',
-        mongoose.Schema(
-            {
-                userName: { type: String, required: true },
-                gender: { type: String, required: true },
-                number: { type: Number, required: true },
-                email: { type: String, required: true },
-                password: { type: String, required: true },
-                isLoggedIn: { type: Boolean, required: true, default: false },
-                token: { type: String, default: '' }
-            },
-            { timestamps: true }
-        )
+  const users = mongoose.model(
+    "users",
+    mongoose.Schema(
+      {
+        userName: { type: String, required: true },
+        gender: { type: String, required: true },
+        number: { type: Number, required: true },
+        email: { type: String, required: true },
+        city: { type: String, required: true },
+        preferences: { type: String, default: "" },
+        password: { type: String, required: true },
+        isLoggedIn: { type: Boolean, required: true, default: false },
+        token: { type: String, default: "" },
+      },
+      { timestamps: true }
     )
+  );
 
-    return users;
-}
+  return users;
+};
