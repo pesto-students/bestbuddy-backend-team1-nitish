ae84cba1d6c654f57e6f4d30e725fe35677e75c4
honeykhra <honeykhra@gmail.com>
2023-04-11 21:27:19 +0530

    user city is now required

--- utils/validation-schema.js
+++ utils/validation-schema.js
@@ -1,37 +1,38 @@
 const userValidator = {
-    rules: {
-        userName: 'required',
-        gender: 'required',
-        number: 'required',
-        email: 'required|email',
-        password: 'required|string|min:8',
-    },
-    customError: {}
-}
+  rules: {
+    userName: "required",
+    gender: "required",
+    number: "required",
+    email: "required|email",
+    city: "required",
+    password: "required|string|min:8",
+  },
+  customError: {},
+};
 
 const loginValidator = {
-    rules: {
-        email: 'required|email',
-        password: 'required',
-    },
-    customError: {}
-}
+  rules: {
+    email: "required|email",
+    password: "required",
+  },
+  customError: {},
+};
 
 const addPropertyValidator = {
-    rules: {
-        name: 'required',
-        city: 'required',
-        phone: 'required',
-        rent: 'required',
-        gender: 'required',
-        category: 'required',
-        availableDate: 'required',
-        typeOfShare: 'required',
-        amenties: 'required',
-        perfrences: 'required',
-        image: 'required'
-    },
-    customError: {}
-}
+  rules: {
+    name: "required",
+    city: "required",
+    phone: "required",
+    rent: "required",
+    gender: "required",
+    category: "required",
+    availableDate: "required",
+    typeOfShare: "required",
+    amenties: "required",
+    preferences: "required",
+    image: "required",
+  },
+  customError: {},
+};
 
-module.exports = { userValidator, loginValidator, addPropertyValidator }
+module.exports = { userValidator, loginValidator, addPropertyValidator };
